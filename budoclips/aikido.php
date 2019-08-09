ddd2961e4175dc6eb28d376222c265d090e9614d
Arkadiusz Paluszek <contact@flori-software.de>
2019-08-09 11:05:30 +0200

    Seite fürs Prüfungsprogramm angelegt

--- budoclips/aikido.php
+++ budoclips/aikido.php
@@ -2,6 +2,7 @@
 include "page_start.php";
 
 echo 'Aikido<p>
+<a href = "pruefungsprogramm.php"> Hier geht es zum <b>Pr&uuml;fungsprogramm</b>!</a>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/398By-Yhf_M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/OPl8PkctT9w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/2yV--AbIz5A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
---
+++ budoclips/pruefungsprogramm.php
@@ -0,0 +1,9 @@
+<?php
+include "page_start.php";
+?>
+    <h1>HALLO WELT</h1>
+
+
+<?php
+include "page_end.php";
+?>
