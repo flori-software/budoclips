79313b127d14655cd9bf33df6982517bfbb97dae
flori-software UG <52501549+flori-software@users.noreply.github.com>
2019-08-12 17:17:11 +0200

    Fortschritt aus dem Master Branch übernommen

--- budoclips/pruefungsprogramm.php
+++ budoclips/pruefungsprogramm.php
@@ -1,9 +1,16 @@
 <?php
 include "page_start.php";
 ?>
-    <h1>HALLO WELT</h1>
+<table rules="all">
+<tr><td>  </td> <td> <b>5.Kyu </td> <td> <b>4.Kyu </td> <td> <b>3.Kyu </td> <td> <b>2.Kyu </td> <td> <b>1.Kyu </td></tr>
+<tr><td> Kata </td> <td> Kamae-Undo </td> <td> Tachi-Suburi 1 </td> <td> Jo-Suburi 1 </td> <td> Tachi-Suburi 2 </td> <td> Jo-Suburi 2 </td></tr>
+<tr><td> Ukemi </td> <td> - Mae-kaiten <br> - Ushiro-kaiten<br> - Yoko-kaiten </td> <td> - Mae-fuse<br> - Ushiro-hineri<br> - Yoko-suberi </td> <td> - Mae-sasae-tobi </td> <td> - Yoko-tobi<br> - Ushiro-tobi </td> <td> - Mae-Tobi (Salto) </td></tr>
+<tr><td colspan = "6"> <b>Tachi-Waza (Mae-Seme) </td></tr>
 
 
+
+
+</table>
 <?php
 include "page_end.php";
 ?>
