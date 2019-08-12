ad3c208b1a86df2c3a4ca4de8c12d923e4768895
flori-software UG <52501549+flori-software@users.noreply.github.com>
2019-08-12 18:50:01 +0200

    Die Tabelle weitergemacht

--- budoclips/pruefungsprogramm.php
+++ budoclips/pruefungsprogramm.php
@@ -2,14 +2,29 @@
 include "page_start.php";
 ?>
 <table rules="all">
-<tr><td>  </td> <td> <b>5.Kyu </td> <td> <b>4.Kyu </td> <td> <b>3.Kyu </td> <td> <b>2.Kyu </td> <td> <b>1.Kyu </td></tr>
-<tr><td> Kata </td> <td> Kamae-Undo </td> <td> Tachi-Suburi 1 </td> <td> Jo-Suburi 1 </td> <td> Tachi-Suburi 2 </td> <td> Jo-Suburi 2 </td></tr>
-<tr><td> Ukemi </td> <td> - Mae-kaiten <br> - Ushiro-kaiten<br> - Yoko-kaiten </td> <td> - Mae-fuse<br> - Ushiro-hineri<br> - Yoko-suberi </td> <td> - Mae-sasae-tobi </td> <td> - Yoko-tobi<br> - Ushiro-tobi </td> <td> - Mae-Tobi (Salto) </td></tr>
+<tr><td>  </td> <td> <b>5.Kyu </td> <td> <b>4.Kyu </td> <td> <b>3.Kyu </td> <td> <b>2.Kyu </td>
+<td> <b>1.Kyu </td></tr>
+
+<tr><td> Kata </td> <td> Kamae-Undo </td> <td> Tachi-Suburi 1 </td> <td> Jo-Suburi 1 </td>
+<td> Tachi-Suburi 2 </td> <td> Jo-Suburi 2 </td></tr>
+
+<tr><td> Ukemi </td> <td><ul>  <li>Mae-kaiten <li>Ushiro-kaiten <li>Yoko-kaiten </ul></td>
+<td><ul> <li>Mae-fuse <li>Ushiro-hineri <li>Yoko-suberi </ul></td> 
+<td><ul>  <li>Mae-sasae-tobi </ul></td> <td><ul> <li>Yoko-tobi <li>Ushiro-tobi </ul></td>
+<td><ul> <li>Mae-Tobi (Salto) </ul></td></tr>
+
 <tr><td colspan = "6"> <b>Tachi-Waza (Mae-Seme) </td></tr>
 
+<tr><td> Katate-Katate-dori<br>(Ai-hammi) </td> <td><ul> <li>Ikkyo <li>Shiho-nage </ul></td>
+<td><ul> <li>Nikyo <li>Juji-nage <li>Irimi-nage <li>Kote-gaeshi </ul></td> 
+<td><ul> <li>Sankyo <li>Sumi-otoshi <li>Kokyu-nage </ul></td> <td><ul> <li>Yonkyo <li>Jiyu-waza </ul></td>
+<td><ul> <li>Ikkyo-Kote-gaeshi <li>Ikkyo-Shiho-nage <li>Koshi-nage(soto-tenkan) </ul></td></tr>
 
-
-
+<tr><td> Katate-Katate-dori<br>(Gyaku-hanmi) </td> <td><ul> <li>Shiho-nage </ul></td> 
+<td><ul> <li>Ikkyo(soto-irimi) <li>Sumi-otoshi <li>Kokyu-nage(irimi) </ul></td>
+<td><ul> <li>Ikkyo<br>(atemi-uchi-tenkan / sukui) <li>Juji-nage<br>(uchi+soto-tenkan) </ul></td>
+<td><ul> <li>Nikyo (2 Arten) <li>Kote-gaeshi <li>Kaiten-nage <li>Jiyu-Waza </ul></td>
+<td><ul> <li>Sankyo (2 Arten) <li>Yonkyo (2 Arten) <li>Irimi-nage (soto-irimi 2 Arten) <li>Kote-gaeshi (hineri) </ul></td>
 </table>
 <?php
 include "page_end.php";
